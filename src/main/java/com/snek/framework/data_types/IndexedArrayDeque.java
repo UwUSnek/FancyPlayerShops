5afd2eacbfad75c32ba3df06f0091f2f11cfee2f
UwU_Snek <xiaoju345@gmail.com>
2025-04-02 19:08:02 +0200

    Add AccessibleArrayDeque class Optimize indexed element access of IndexedArrayDeque class

--- src/main/java/com/snek/framework/data_types/IndexedArrayDeque.java
+++ src/main/java/com/snek/framework/data_types/IndexedArrayDeque.java
@@ -1,22 +1,15 @@
 package com.snek.framework.data_types;
 
-import java.lang.reflect.Field;
-import java.util.ArrayDeque;
 
 
 
 
 
-
-//FIXME make index access O(1)
-//FIXME make index access O(1)
-//FIXME make index access O(1)
-//FIXME make index access O(1)
 
 /**
- * An ArrayDequeue that allows indexing in O(n) time instead of the O(1) it should have been because Java is stupid.
+ * An ArrayDequeue that allows indexing in O(1) time.
  */
-public class IndexedArrayDeque<E> extends ArrayDeque<E> {
+public class IndexedArrayDeque<E> extends AccessibleArrayDeque<E> {
     public IndexedArrayDeque() {
         super();
     }
@@ -28,19 +21,12 @@ public class IndexedArrayDeque<E> extends ArrayDeque<E> {
      * @param index The index.
      * @return The element.
      */
+    @SuppressWarnings("unchecked")
     public E get(int index) {
         if (index < 0 || index >= size()) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
         }
 
-        int i = 0;
-        for (E element : this) {
-            if (i == index) {
-                return element;
-            }
-            i++;
-        }
-
-        throw new IllegalStateException("Element not found, this should never happen.");
+        return (E)elements[(head + index) & (elements.length - 1)];
     }
 }
